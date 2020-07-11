3dd0ace46d19af34d616658fb6490c5a4be15e6a
Chad Lengefeld <chadlengefeld@gmail.com>
2020-07-11 03:15:06 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -26,13 +26,12 @@ class LandmarksController < ApplicationController
     @landmark.name = params['landmark']['name'])
     @landmark.year_completed = params['landmark']['year_completed']
     @landmark.save
-    redirect ("/landmarks/#{@landmark.id}")
+    redirect "/landmarks/#{@landmark.id}"
   end
 
 
   post '/landmarks' do
-    Landmark.create(name: params['landmark']['name'])
-    Landmark << year_completed: params['landmark']['year_completed']
+    Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'] )
       redirect '/landmarks'
     end
 end
