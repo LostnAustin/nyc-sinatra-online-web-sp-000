deed54fb85553d7957bfaade2b836852cad3cf7f
Chad Lengefeld <chadlengefeld@gmail.com>
2020-07-11 03:13:01 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -31,7 +31,8 @@ class LandmarksController < ApplicationController
 
 
   post '/landmarks' do
-    Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'] )
+    Landmark.create(name: params['landmark']['name'])
+    Landmark << year_completed: params['landmark']['year_completed']
       redirect '/landmarks'
     end
 end
