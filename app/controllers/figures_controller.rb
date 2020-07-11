b2a11ae450d08b054e7bed45fc1c3c0276bb5541
Chad Lengefeld <chadlengefeld@gmail.com>
2020-07-11 04:04:10 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -35,7 +35,7 @@ class FiguresController < ApplicationController
     erb :'figures/edit'
   end
 
-  post '/figures/:id' do
+  patch '/figures/:id' do
     @figure = Figure.find(params[:id])
     @figure.update(params[:figure])
       unless params[:title][:name].empty?
