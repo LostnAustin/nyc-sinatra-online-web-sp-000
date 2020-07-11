074fe036ccdbb02e0146a185f056625e8ec048d9
Chad Lengefeld <chadlengefeld@gmail.com>
2020-07-11 04:17:59 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -24,26 +24,30 @@ class LandmarksController < ApplicationController
   post '/landmarks/:id' do
     Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
    redirect '/landmarks'
-    # @landmark = Landmark.find(params[:id])
-    # @landmark.name = params['landmark']['name']
-    # @landmark.year_completed = params['landmark']['year_completed']
-    # @landmark.save
-    # redirect "/landmarks/#{@landmark.id}"
+    @landmark = Landmark.find(params[:id])
+    @landmark.name = params['landmark']['name']
+    @landmark.year_completed = params['landmark']['year_completed']
+    @landmark.save
+    redirect "/landmarks/#{@landmark.id}"
   end
 
 
-  patch '/landmarks/:id' do
-    @landmark = Landmark.find(params[:id])
-    @landmark.update(params[:landmark])
-      unless params[:landmark][:name].empty?
-        @landmark.name << Landmark.create(params[:landmark][:name])
-      end
-      unless params[:landmark][:year_completed].empty?
-        @landmark.year_completed << Landmark.create(params[:landmark][:year_completed])
-      end
-        @landmark.save
-    redirect "/landmarks/#{@landmark.id}"
-    end
+  post '/landmarks/' do
+    Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
+   redirect '/landmarks'
+ end
+  # patch '/landmarks/:id' do
+  #   @landmark = Landmark.find(params[:id])
+  #   @landmark.update(params[:landmark])
+  #     unless params[:landmark][:name].empty?
+  #       @landmark.name << Landmark.create(params[:landmark][:name])
+  #     end
+  #     unless params[:landmark][:year_completed].empty?
+  #       @landmark.year_completed << Landmark.create(params[:landmark][:year_completed])
+  #     end
+  #       @landmark.save
+  #   redirect "/landmarks/#{@landmark.id}"
+  #   end
 
 
 
