20fd472da4f6808c0321078c717d1742c51ecfa5
Chad Lengefeld <chadlengefeld@gmail.com>
2020-07-11 02:54:32 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,6 +1,37 @@
 class LandmarksController < ApplicationController
   get '/landmarks' do
+    # binding.pry
     @landmarks = Landmark.all
+    @figures = Figure.all
+
     erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :"/landmarks/new"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params["landmark"])
+      unless params[:figure][:name].empty?
+        @figure.names << Name.create(params[:name])
+      end
+      unless params[:title][:name].empty
+        @figure.titles << Title.create(params[:title])
+      end
+      @figure.save
+
+    redirect ("/landmarks/#{@landmark.id}")
   end
 end
